14086150bf816df830868f516b5d054824668d46
99002773 <72495902+99002773@users.noreply.github.com>
2020-11-20 18:20:51 +0530

    Update DbAccessObj.java
    Successfully committed Database Access file

--- app/src/main/java/com/example/latestnewsapp/database/DbAccessObj.java
+++ app/src/main/java/com/example/latestnewsapp/database/DbAccessObj.java
@@ -1,26 +1,20 @@
 package com.example.latestnewsapp.database;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-
+import android.widget.Toast;
 import com.example.latestnewsapp.database.FeedReaderContract.FeedEntry;
-
 public class DbAccessObj {
-
     SQLiteDatabase database;
     DbHelper dbHelper;
-
     public DbAccessObj(Context context) {
         dbHelper = new DbHelper(context);
     }
-
     public void openDb(){
         database = dbHelper.getWritableDatabase();
     }
     private void closeDb(){}
-
     public void createRow(String title, String subtitle){
         ContentValues values = new ContentValues();
         values.put(FeedEntry.COLUMN_NAME_TITLE,title);
@@ -36,30 +30,51 @@ public class DbAccessObj {
         cursor.moveToPosition(a);
         int titleIndex = cursor.getColumnIndexOrThrow(FeedEntry.COLUMN_NAME_TITLE);
         int subtitleIndex = cursor.getColumnIndexOrThrow(FeedEntry.COLUMN_NAME_SUBTITLE);
-
         String title = cursor.getString(titleIndex);
         String subtitle = cursor.getString(subtitleIndex);
         return title +"\n" +subtitle;
     }
     private void updateRow(){}
     private void deleteRow(){}
-
     public Cursor getRows() {
         Cursor cursor = database.query(FeedEntry.TABLE_NAME,null,null,null,null,null,null);
         return cursor;
     }
 
-
-    public String query(String queryParam) {
-
-
+    public String  query(String queryParam) {
+        String table = FeedEntry.TABLE_NAME;
+        String[] columns = {FeedEntry.COLUMN_NAME_TITLE,FeedEntry.COLUMN_NAME_SUBTITLE}; //projection = columns
+        String selection = FeedEntry.COLUMN_NAME_TITLE +" =?"; //selection = rows
+        String[] selectionArgs = {queryParam};
+        String groupBy = null;
+        String having = null;
+        String orderBy = FeedEntry.COLUMN_NAME_TITLE+" DESC";
+        String limit = "10";
+        Cursor cursor = database.query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+        int subtitleIndex = cursor.getColumnIndexOrThrow(FeedEntry.COLUMN_NAME_SUBTITLE);
+        cursor.moveToLast();
+        String subtitle = cursor.getString(subtitleIndex);
+        return subtitle;
     }
 
-
     public String uservalid(String queryParam) {
-
-
+        String table = FeedEntry.TABLE_NAME;
+        String[] columns = {FeedEntry.COLUMN_NAME_TITLE, FeedEntry.COLUMN_NAME_SUBTITLE}; //projection = columns
+        String selection = FeedEntry.COLUMN_NAME_TITLE + " =?"; //selection = rows
+        String[] selectionArgs = {queryParam};
+        String groupBy = null;
+        String having = null;
+        String orderBy = FeedEntry.COLUMN_NAME_TITLE + " DESC";
+        String limit = "10";
+        String register = "Please Register";
+        Cursor cursor = database.query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+        if (cursor.getCount() >0) {
+            int titleIndex = cursor.getColumnIndexOrThrow(FeedEntry.COLUMN_NAME_TITLE);
+            cursor.moveToLast();
+            String title = cursor.getString(titleIndex);
+            return title;
+        }
+       return "a";
 }
-
 
 }
