a9981992cda7c30f5b2f0e24ea12a8fa85c1b2b9
99002762 <72461476+99002762@users.noreply.github.com>
2020-11-20 17:48:51 +0530

    Update NewsDetailActivity.java
    News details Code added

--- app/src/main/java/com/example/latestnewsapp/NewsDetailActivity.java
+++ app/src/main/java/com/example/latestnewsapp/NewsDetailActivity.java
@@ -1,12 +1,14 @@
 package com.example.latestnewsapp;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.net.Uri;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
@@ -15,6 +17,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -145,6 +148,30 @@ public class NewsDetailActivity extends AppCompatActivity implements AppBarLayou
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+        int id = item.getItemId();
 
+        if (id == R.id.view_web){
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(mUrl));
+            startActivity(i);
+            return true;
+        }
+
+        else if (id == R.id.share){
+            try{
+
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plan");
+                i.putExtra(Intent.EXTRA_SUBJECT, mSource);
+                String body = mTitle + "\n" + mUrl + "\n" + "Share from the News App" + "\n";
+                i.putExtra(Intent.EXTRA_TEXT, body);
+                startActivity(Intent.createChooser(i, "Share with :"));
+
+            }catch (Exception e){
+                Toast.makeText(this, "Hmm.. Sorry, \nCannot be share", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
